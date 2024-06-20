d267f1bfb86390294459c59750547090601eb29a
Shivam Singh <hackerxeroid@outlook.com>
2024-06-20 21:54:24 +0530

    Version 2 addedd

--- src/App.js
+++ src/App.js
@@ -48,23 +48,62 @@ function App() {
   return (
     <div className="container mx-auto mt-3">
       <div className="flex flex-col items-center">
-        <div className="mb-4">
-          <StepComponent
-            label="Step"
-            counter={step}
-            onIncrement={() => setStep((step) => step + 1)}
-            onDecrement={() => setStep((step) => step - 1)}
-          />
+        <div className="mb-4 flex flex-col items-center gap-2">
+          <div className="flex items-center gap-2">
+            <input
+              type="range"
+              name="step"
+              id="step"
+              min="0"
+              max="10"
+              value={step}
+              onChange={(e) => setStep(+e.target.value)}
+            />
+            <span>{step}</span>
+          </div>
+          <div>
+            <button
+              onClick={() => setCounter((prevCounter) => prevCounter - step)}
+              className="border-2 border-blue-400 bg-blue-400 px-2"
+            >
+              -
+            </button>
+            <input
+              className="border-2 border-blue-400 outline-none"
+              type="number"
+              name="counter"
+              id="counter"
+              value={counter}
+              onChange={(e) => {
+                console.log(e.target.value);
+                setCounter((prevCounter) => {
+                  if (isNaN(e.target.value)) return prevCounter;
 
-          <StepComponent
-            label="Count"
-            counter={counter}
-            onIncrement={() => setCounter((counter) => counter + step)}
-            onDecrement={() => setCounter((counter) => counter - step)}
-          />
+                  return +e.target.value;
+                });
+              }}
+            />
+            <button
+              onClick={() => setCounter((prevCounter) => prevCounter + step)}
+              className="border-2 border-blue-400 bg-blue-400 px-2"
+            >
+              +
+            </button>
+          </div>
         </div>
 
-        <p className=" font-bold">{addDaysToDate(counter, Date.now())}</p>
+        <p className=" font-bold mb-4">{addDaysToDate(counter, Date.now())}</p>
+        {(step !== 1 || counter !== 0) && (
+          <button
+            onClick={() => {
+              setStep(1);
+              setCounter(0);
+            }}
+            className="px-6 py-2 bg-blue-400 rounded-md"
+          >
+            Reset
+          </button>
+        )}
       </div>
     </div>
   );
